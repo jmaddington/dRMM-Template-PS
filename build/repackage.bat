f16d502e48205f964417acbd635be474919df428
jmaddington <jm@jmaddington.com>
2018-12-07 13:48:52 -0500

    Updated packaging component to include everything but tools specificl to the build process

--- build/repackage.bat
+++ build/repackage.bat
@@ -1,5 +1,8 @@
+@REM This will package everything in the parent folder EXCEPT the build components themselves.
+@REM Relative paths will be preserved
+
 cd ..
 copy build\resource.xml
 del aem-component.cpt
-.\bin\7zip\7z.exe a  aem-component.cpt -tzip command.bat resource.xml
+.\bin\7zip\7z.exe a  aem-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/
 del resource.xml
