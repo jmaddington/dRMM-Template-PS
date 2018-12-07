e21f8f4f6b26bf20d33acabd83f17dc874c91e77
jmaddington <jm@jmaddington.com>
2018-12-07 15:05:24 -0500

    Rename command.bat to command.ps1, update build script so it still works

--- build/repackage.bat
+++ build/repackage.bat
@@ -3,6 +3,8 @@
 
 cd ..
 copy build\resource.xml
+copy command.ps1 command.bat
 del aem-component.cpt
 .\bin\7zip\7z.exe a  aem-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/
 del resource.xml
+del command.bat
