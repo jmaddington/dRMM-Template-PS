c3b1b82176c3632222e957af219d1d6d332e30ee
Jonathan Addington <jonathan.addington@jmaddington.com>
2024-08-20 14:04:05 -0400

    chore: Exclude bin folder from packaging in repackage.bat

--- build/repackage.bat
+++ build/repackage.bat
@@ -1,10 +1,10 @@
-@REM This will package everything in the parent folder EXCEPT the build components themselves.
+@REM This will package everything in the parent folder EXCEPT the build components themselves, and the bin folder
 @REM Relative paths will be preserved
 
 cd ..
 copy build\resource.xml
 copy command.ps1 command.bat
 del aem-component.cpt
-.\bin\7zip\7z.exe a  aem-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/
+.\bin\7zip\7z.exe a  aem-component.cpt -tzip * -x!.git/ -x!initialize/ -x!build/ -x!bin/
 del resource.xml
 del command.bat
